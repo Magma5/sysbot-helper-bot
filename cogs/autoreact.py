d1c814eb49cc3a758f3b5276d1f15efadcc7c73e
Magma5 <mosciskistan@outlook.com>
2022-03-23 21:04:51 +0000

    New version of autoreact (incompatible!)
    Autoreact has been updated, and old config files are incompatible.
    Note the new version is still in ALPHA and is subject to change!
    * The new version is much faster and flexible, with full async checking
      and reacting.

--- cogs/autoreact.py
+++ cogs/autoreact.py
@@ -1,201 +1,278 @@
-import asyncio
-from contextlib import suppress
 import re
-from typing import Optional, Union
-from discord.errors import HTTPException
+import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+from discord.ext.commands.context import Context
 from discord.message import Message
+
+from .utils import wait_tasks_any, wait_tasks_all
 
 
 @dataclass
-class ReactConfig:
-    channel: Optional[Union[int, list[int]]] = None
-    guild: Optional[Union[int, list[int]]] = None
+class ReactMatcher:
+    def __init__(self, bot, embeds_check_delay=1.5):
+        self._bot = bot
+        self.match_groups = {}
+        self.embeds_check_delay = embeds_check_delay
 
-    match_media: bool = False
-    match_content: Optional[str] = None
-    match_content_type: Optional[str] = None
-    match_file_name: Optional[str] = None
-    match_all: bool = False
+    def _re_match(self, pattern: re.Pattern, text: str):
+        if not isinstance(pattern, re.Pattern):
+            pattern = re.compile(pattern)
+        result = pattern.match(text)
+        if result:
+            self.match_groups.update(result.groupdict())
+        return result
 
-    react_with: list[int] = field(default_factory=list)
-    send_with: list[str] = field(default_factory=list)
-    reply_with: list[str] = field(default_factory=list)
-    delete: bool = False
-
-    ignore_bots: bool = True
-    embed_check_delay: float = 1.5
-
-    # def __post_init__(self):
-    #     if not isinstance(self.react_with, list):
-    #         self.react_with = [self.react_with]
-    #     if not isinstance(self.send_with, list):
-    #         self.react_with = [self.react_with]
-
-    @classmethod
-    def _get_list(self, var):
-        if not isinstance(var, list):
-            return [var]
-        return var
-
-    @property
-    def react_with_list(self):
-        return self._get_list(self.react_with)
-
-    @property
-    def send_with_list(self):
-        return self._get_list(self.send_with)
-
-    @property
-    def reply_with_list(self):
-        return self._get_list(self.reply_with)
-
-    @classmethod
-    async def check_content_type(cls, message: Message, pattern: re.Pattern):
+    async def content_type(self, message: Message, pattern: str):
         """Check if any attachment matches content type regex."""
         if not message.attachments:
             return False
 
         for attachment in message.attachments:
-            if attachment.content_type and pattern.match(attachment.content_type):
+            if attachment.content_type and self._re_match(pattern, attachment.content_type):
                 return True
         return False
 
-    async def check_match_media(self, message: Message):
+    async def media(self, message: Message):
         """Check if the message contains an image or video (attachments or embeds)"""
-        if not self.match_media:
-            return False
-
-        content_type_pattern = re.compile(r'^(image|video)/.+', re.IGNORECASE)
-        if await self.check_content_type(message, content_type_pattern):
+        content_type_pattern = re.compile(r'^(image|video)/', re.IGNORECASE)
+        if await self.content_type(message, content_type_pattern):
             return True
 
         # It take a bit of time for user embeds to show up.
         # Wait a little before checking the embeds.
-        await asyncio.sleep(self.embed_check_delay)
-        if message.embeds:
-            if any(e.image or e.video or e.thumbnail for e in message.embeds):
+        if not await self.embeds(message):
+            return False
+
+        return any(e.image or e.video for e in message.embeds)
+
+    async def embeds(self, message: Message):
+        for _ in range(int(self.embeds_check_delay / 0.1)):
+            if message.embeds:
                 return True
+            await asyncio.sleep(0.1)
         return False
 
-    async def check_match_content(self, message: Message):
+    async def content(self, message: Message, pattern: str):
         """Check if the message content matches regex."""
-        if not self.match_content:
-            return False
-        content_pattern = re.compile(self.match_content, re.DOTALL | re.IGNORECASE)
-        if content_pattern.match(message.content):
-            return True
-        return False
+        content_pattern = re.compile(pattern, re.DOTALL | re.IGNORECASE)
+        return self._re_match(content_pattern, message.content) is not None
 
-    async def check_match_content_type(self, message: Message):
-        """Check if the attachment content type matches regex."""
-        if not self.match_content_type:
-            return False
-        return self.check_content_type(message, re.compile(self.match_content_type))
-
-    async def check_match_file_name(self, message: Message):
+    async def attachment_name(self, message: Message, pattern: str):
         """Check if attachment file name matches regex."""
-        if not self.match_file_name:
+        if not message.attachments:
             return False
 
+        pattern = re.compile(pattern, re.DOTALL | re.IGNORECASE)
         for attachment in message.attachments:
-            if re.match(self.match_file_name, attachment.content_type, re.IGNORECASE):
+            if self._re_match(pattern, attachment.filename):
                 return True
         return False
 
-    async def check_match_all(self, _):
-        return self.match_all
+    async def bot(self, message: Message):
+        return message.author.bot
 
-    def check_match_channel(self, message: Message):
-        """Check if message is sent from the correct channel."""
-        if isinstance(self.channel, list):
-            return message.channel.id in self.channel
-        return message.channel.id == self.channel
+    async def has_permission(self, message: Message, permission):
+        perm = message.author.guild_permissions
+        if permission not in perm.VALID_FLAGS:
+            raise ValueError(f'{permission} is not a valid permission!')
+        return getattr(perm, permission) is not False
 
-    def check_match_guild(self, message: Message):
-        """Check if message is sent from the correct guild."""
-        if not self.guild:
-            return False
-        if isinstance(self.guild, list):
-            return message.guild.id in self.guild
-        return message.guild.id == self.guild
+    async def channel(self, message: Message, channel_id: int):
+        return message.channel.id == channel_id
 
-    async def check_match(self, message: Message):
-        """Main checking method, check message against config."""
+    async def channels(self, message: Message, channel_id: int):
+        return await self.channel(message, channel_id)
 
-        # If channel doesn't match, reject directly
-        if not (self.check_match_channel(message) or self.check_match_guild(message)):
-            return False
+    async def guild(self, message: Message, guild_id: int):
+        return message.guild.id == guild_id
 
-        if self.ignore_bots and message.author.bot:
-            return False
+    async def guilds(self, message: Message, guild_id: int):
+        return await self.guild(message, guild_id)
 
-        for check in [self.check_match_all,
-                      self.check_match_media,
-                      self.check_match_content,
-                      self.check_match_content_type,
-                      self.check_match_file_name]:
-            if await check(message):
-                return True
+    async def author(self, message: Message, author_id: int):
+        return message.author.id == author_id
 
-        return False
+    async def authors(self, message: Message, author_id: int):
+        return await self.author(message, author_id)
 
-    async def do_actions(self, ctx):
-        template_env = self.bot.template_env
-        variables = self.bot.template_variables(ctx)
+    async def mentions(self, message: Message, user_id: int):
+        return user_id in message.raw_mentions
 
-        await asyncio.sleep(1.5)
+    async def mentions_role(self, message: Message, role_id: int):
+        return role_id in message.raw_role_mentions
 
-        for emoji in self.react_with_list:
-            if isinstance(emoji, int):
-                emoji = ctx.bot.get_emoji(emoji)
-            with suppress(HTTPException):
-                await ctx.message.add_reaction(emoji)
+    async def mentions_channel(self, message: Message, channel_id: int):
+        return channel_id in message.raw_channel_mentions
 
-        # Reply
-        for message in self.reply_with_list:
-            with suppress(HTTPException):
-                await ctx.reply(template_env.from_string(message).render(variables))
+    async def any(self, _):
+        return True
 
-        # Send
-        for message in self.send_with_list:
-            with suppress(HTTPException):
-                await ctx.send(template_env.from_string(message).render(variables))
+    async def all(self, _):
+        return True
 
-        # Delete
-        if self.delete:
-            with suppress(HTTPException):
-                await ctx.message.delete()
+
+class ReactAction:
+    def __init__(self, ctx: Context, matcher: ReactMatcher):
+        self.ctx = ctx
+        self.bot = ctx.bot
+        self.template_env = self.bot.template_env
+        self.variables = self.bot.template_variables(ctx)
+        self.variables['groups'] = matcher.match_groups
+        self.sent_messages = []
+
+    async def react(self, emoji):
+        if isinstance(emoji, int):
+            emoji = self.bot.get_emoji(emoji)
+        await self.ctx.message.add_reaction(emoji)
+
+    async def reply(self, text):
+        msg = await self.ctx.reply(
+            self.template_env.from_string(text).render(self.variables))
+        self.sent_messages.append(msg)
+
+    async def send(self, text):
+        msg = await self.ctx.send(
+            self.template_env.from_string(text).render(self.variables))
+        self.sent_messages.append(msg)
+
+    async def delete(self, yes=True):
+        if yes:
+            await self.ctx.message.delete()
+
+    async def delete_after(self, delay):
+        await asyncio.sleep(delay)
+        await self.delete()
+
+    async def delete_replies_after(self, delay):
+        await asyncio.sleep(delay)
+        while self.sent_messages:
+            msg = self.sent_messages.pop()
+            await msg.delete()
+
+    async def delay(self, delay):
+        await asyncio.sleep(delay)
+
+    async def suppress_embeds(self, yes):
+        if yes:
+            await self.ctx.message.edit(suppress=True)
+
+
+@dataclass
+class ReactConfig:
+    def __init__(self, **kwargs):
+        # General configs
+        self.embeds_check_delay = kwargs.pop('embeds_check_delay', 1.5)
+
+        # Define default rules
+        self.rules = {
+            'bot': False
+        }
+
+        # Populate rules dict by checking the keys
+        for k in list(kwargs.keys()):
+            k_ = k.lower()
+            if k.endswith('!'):
+                k_ = k[:-1]
+            if k_ == 'match' or hasattr(ReactMatcher, k_):
+                self.rules[k] = kwargs.pop(k)
+
+        # The remaining keys are all actions
+        self.actions = self._ensure_list(kwargs.pop('actions', dict()))
+        self.actions.insert(0, dict(**kwargs))
+
+    async def match_helper_bool(self, fn, message, expected):
+        return await fn(message) is expected
+
+    async def match_helper_inverted(self, fn, message, arg):
+        return not await fn(message, arg)
+
+    def get_match_helper_rules(self, matcher):
+        async def run_match_match(message, rules):
+            tasks = []
+            for name, args in rules.items():
+                args = self._ensure_list(args)
+                coro = self.match_item(matcher, name, message, *args)
+                tasks.append(asyncio.create_task(coro))
+            return await wait_tasks_all(tasks)
+        return run_match_match
+
+    async def match_item(self, matcher, name, message, *args):
+        # Check if operation is inverted
+        inverted = False
+        if name.endswith('!'):
+            inverted = True
+            name = name[:-1]
+
+        if name == 'match':
+            fn = self.get_match_helper_rules(matcher)
+        else:
+            fn = getattr(matcher, name)
+
+        tasks = []
+        for arg in args:
+            # Set value to null to skip the check entirely
+            if arg is None:
+                continue
+
+            if isinstance(arg, bool):
+                coro = self.match_helper_bool(fn, message, arg != inverted)
+            elif inverted:
+                coro = self.match_helper_inverted(fn, message, arg)
+            else:
+                coro = fn(message, arg)
+            tasks.append(asyncio.create_task(coro))
+        return await wait_tasks_any(tasks)
+
+    def _ensure_list(self, arg):
+        if isinstance(arg, list):
+            return arg
+        return [arg]
+
+    async def check_match(self, bot, message: Message):
+        matcher = ReactMatcher(bot)
+        if await self.match_item(matcher, 'match', message, self.rules):
+            return matcher
+
+    async def do_actions(self, ctx, matcher):
+        react_action = ReactAction(ctx, matcher)
+        for actions in self.actions:
+            coro_list = []
+            for name, args in actions.items():
+                for action_arg in self._ensure_list(args):
+                    fn = getattr(react_action, name)
+                    coro_list.append(fn(action_arg))
+
+            if coro_list:
+                await asyncio.gather(*coro_list)
 
 
 class Autoreact(commands.Cog):
     """Reacts to message in a specified channel"""
 
     class Config:
-        def __init__(self, *channels):
-            self.channels = []
-            for channel_config in channels:
-                cfg = ReactConfig(**channel_config)
-                self.channels.append(cfg)
+        def __init__(self, *rules):
+            self.react_configs = [ReactConfig(**rule) for rule in rules]
 
     def __init__(self, bot, config):
         self.bot: Bot = bot
         self.config = config
 
-        for channel in self.config.channels:
-            channel.bot = bot
+    async def do_auto_react(self, message, react_config: ReactConfig):
+        """Run auto react for a single rule."""
+        matcher = await react_config.check_match(self.bot, message)
+        if matcher:
+            # Get context when message matches
+            ctx = await self.bot.get_context(message)
+            await react_config.do_actions(ctx, matcher)
 
     @commands.Cog.listener()
     async def on_message(self, message: Message):
+        # Filter out message by itself
         if message.author == self.bot.user:
             return
 
-        # The get context will be called for every single message
-        ctx = await self.bot.get_context(message)
-
-        for react_config in self.config.channels:
-            if await react_config.check_match(message):
-                await react_config.do_actions(ctx)
+        await asyncio.gather(*(self.do_auto_react(message, config)
+                               for config in self.config.react_configs))
