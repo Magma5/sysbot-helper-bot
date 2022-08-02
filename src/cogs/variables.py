47dcf07e55128dd3463281b32103147d46d1ff81
Magma5 <mosciskistan@outlook.com>
2022-08-02 21:45:07 +0000

    Add set variable command

--- src/cogs/variables.py
+++ src/cogs/variables.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from .checks import is_sudo
+
 
 class Variables(commands.Cog):
     """This cog registers template variables only and does not register any commands."""
@@ -14,3 +16,15 @@ class Variables(commands.Cog):
 
     def template_variables(self, _):
         return self.config.variables
+
+    @commands.command()
+    @is_sudo()
+    async def setvariable(self, ctx, name: str, value: str):
+        if name in self.config.variables:
+            if not isinstance(self.config.variables[name], str):
+                await ctx.send(f"Cannot set variable: {name} is not a string!")
+                return
+            await ctx.send(f'Variable [{name}] set to "{value}"')
+        else:
+            await ctx.send(f'Created new variable [{name}], set to "{value}"')
+        self.config.variables[name] = value
